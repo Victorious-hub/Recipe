b8085a715b7a75c427f567bb87a8d8f1107e01fd
Shyskov <shyskov@inbox.ru>
2024-09-11 10:42:45 +0300

    Fixed data schema on pydantic. Updated Handler class and client

--- api/dependencies.py
+++ api/dependencies.py
@@ -8,11 +8,11 @@ from dependency_injector import containers, providers
 class Container(containers.DeclarativeContainer):
     """IoC container for dependency injection."""
 
-    reqeust_handlers = {
+    handlers = {
         HandlerType.USER_ID_HANDLER_FIELD.value: UserIdFieldHandler(),
         HandlerType.COOKING_TIME_HANDLER_FIELD.value: CookingTimeHandler(),
     }
     base_api_url = "https://dummyjson.com/recipes"
-    client = providers.Singleton(Client, base_url=base_api_url, handlers=reqeust_handlers)
+    client = providers.Singleton(Client, base_url=base_api_url, handlers=handlers)
 
-    mock_client = providers.Singleton(MockClient, base_url=base_api_url, handlers=reqeust_handlers)
+    mock_client = providers.Singleton(MockClient, base_url=base_api_url, handlers=handlers)
